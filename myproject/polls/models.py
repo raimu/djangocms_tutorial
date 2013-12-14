8f8ac7f7b1c073bf3cba451739fab2a530d850fc
raimu <ortwang@googlemail.com>
2013-12-14 22:35:02 +0000

    register polls as cms-plugin

--- myproject/polls/models.py
+++ myproject/polls/models.py
@@ -2,6 +2,7 @@
 import datetime
 from django.utils import timezone
 from django.db import models
+from cms.models import CMSPlugin
 
 
 class Poll(models.Model):
@@ -26,3 +27,11 @@ class Choice(models.Model):
 
     def __unicode__(self):  # Python 3: def __str__(self):
         return self.choice_text
+
+
+class PollPlugin(CMSPlugin):
+    poll = models.ForeignKey('polls.Poll', related_name='plugins')
+
+    def __unicode__(self):
+        return self.poll.question
+
